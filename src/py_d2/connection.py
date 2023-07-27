d41f346c8ba7dfaac312d8a99805540193092eef
Faris L Kapes <flk203@exeter.ac.uk>
2023-07-27 11:25:10 +0200

    Update connection.py

--- src/py_d2/connection.py
+++ src/py_d2/connection.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from enum import Enum
 from typing import List
+from helpers import add_weights_for_max_flow
 from typing import Optional
 
 
@@ -12,13 +13,16 @@ class Direction(Enum):
 
 
 class D2Connection:
-    def __init__(self, shape_1: str, shape_2: str, label: Optional[str] = None, direction: Direction = Direction.TO):
+    def __init__(self, shape_1: str, shape_2: str, label: Optional[str] = None, direction: Direction = Direction.TO, weight: Optional[int] = None]):
         self.shape_1 = shape_1
         self.shape_2 = shape_2
         self.label = label
         self.direction = direction
 
     def lines(self) -> List[str]:
+        if self.direction == Direction.BOTH and weight != None:
+            base = add_weights_for_max_flow(self.shape_1, self.shape_2, [weight[0], weight[1]])
+            return [base]
         base = f"{self.shape_1} {self.direction.value} {self.shape_2}"
         if self.label:
             base += f": {self.label}"
